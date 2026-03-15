d4a2702136b6e7e548cc69dfb8f2da0744827936
Diego <cdtoruno@uamv.edu.ni>
2026-03-15 01:31:22 -0600

    fix: update STATIC_ROOT path for deployment compatibility

--- backend/gym_payroll/settings.py
+++ backend/gym_payroll/settings.py
@@ -118,7 +118,7 @@ USE_I18N      = True
 USE_TZ        = True
 
 STATIC_URL  = "/static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = "/app/staticfiles"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
